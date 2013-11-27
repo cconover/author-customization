23d2c064460fd6e033dc67a6085abd6e0c802ede
Christiaan Conover <dev@cconover.com>
2013-11-27 14:44:53 -0500

    Added locations for meta box to be shown
    Allow meta box to be shown on pages in addition to posts. Raised
    priority of metabox to ‘high’

--- author-customization/admin/includes/edit-post.php
+++ author-customization/admin/includes/edit-post.php
@@ -18,7 +18,12 @@ Functions to include when editing a post
  */
 /* Add meta box to Edit Post */
 function cc_author_add_metabox() {
-	add_meta_box( 'custom-author-complete', 'Authors', 'cc_author_metabox', 'post' ); // Parameters for adding meta box
+	$screens = array( 'post', 'page' ); // Locations where the metabox should show
+	
+	/* Iterate through locations */
+	foreach( $screens as $screen ) {
+		add_meta_box( 'cc-author-metabox', 'Author', 'cc_author_metabox', $screen, 'normal', 'high' ); // Parameters for adding meta box
+	}
 } // cc_author_add_metabox()
 add_action( 'add_meta_boxes', 'cc_author_add_metabox' ); // Hook meta box into WordPress
 
