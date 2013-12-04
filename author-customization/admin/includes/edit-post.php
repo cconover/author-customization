fb6d77b1a465e4bce9ad49bfc4d8166b179896db
Christiaan Conover <dev@cconover.com>
2013-12-04 16:41:59 -0500

    Created meta box display code
    Added the HTML to display the elements of the author meta box

--- author-customization/admin/includes/edit-post.php
+++ author-customization/admin/includes/edit-post.php
@@ -20,7 +20,37 @@ add_action( 'add_meta_boxes', 'cc_author_add_metabox' ); // Hook meta box into W
 
 /* Meta box code: $post is the data for the current post */
 function cc_author_metabox( $post ) {
-	echo 'This is the Author meta box.';
+	/* Retrieve current values if they exist */
+	$cc_author_displayname = get_post_meta( $post->ID, 'cc_author_displayname', true ); // Author display name
+	$cc_author_bio = get_post_meta( $post->ID, 'cc_author_bio', true ); // Author bio
+	
+	/* If any of the values are missing from the post, retrieve them from the author's global profile */
+	if ( !isset( $cc_author_displayname ) ) {
+		
+	}
+	
+	/* Display the meta box contents */
+	echo 'The following author information will be saved for this post.';
+	?>
+	<div id="wrap">
+		<table class="form-table">
+			<tbody>
+				<tr valign="top">
+					<th scope="row">Name</th>
+					<td>
+						<input type="text" name="cc_author_displayname" value="<?php echo esc_attr( $cc_author_displayname ); ?>" size="<?php strlen( $cc_author_displayname ); ?>" />
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">Bio</th>
+					<td>
+						<textarea name="cc_author_bio" rows="5" cols="50" required><?php echo esc_attr( $cc_author_bio ); ?></textarea>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<?php
 }
 /**
  * End Author Meta Box
