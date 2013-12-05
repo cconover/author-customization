d003c72bde311296d3061eee80f69661c09c478e
Christiaan Conover <dev@cconover.com>
2013-12-05 17:15:27 -0500

    Switched meta storage to array
    Switched the storage of author meta data from separate database entries
    to a single entry inside an array. Sets the plugin up for better future
    backward compatibility.

--- author-customization/admin/includes/edit-post.php
+++ author-customization/admin/includes/edit-post.php
@@ -30,30 +30,39 @@ add_action( 'add_meta_boxes', 'cc_author_add_metabox' ); // Hook meta box update
 /* Meta box code: $post is the data for the current post */
 function cc_author_metabox( $post ) {
 	/* Retrieve current values if they exist */
-	$cc_author_displayname = get_post_meta( $post->ID, 'cc_author_displayname', true ); // Author display name
-	$cc_author_bio = get_post_meta( $post->ID, 'cc_author_bio', true ); // Author bio
+	$cc_author_meta = get_post_meta( $post->ID, '_cc_author_meta', true ); // Author meta data (stored as an array)
 	
 	/* If any of the values are missing from the post, retrieve them from the author's global profile */
-	if ( !$cc_author_displayname || !$cc_author_bio ) {
+	if ( !$cc_author_meta ) {
 		$currentuserid = get_current_user_id(); // Get the user ID of the current user
 		
 		$currentuser = get_userdata( $currentuserid ); // Retrieve the details of the current user
 		
-		$cc_author_displayname = $currentuser->display_name; // Set display name from current user's data
-		$cc_author_bio = $currentuser->description; // Set bio from the current user's data
+		$cc_author_meta = array(); // Initialize array
+		$cc_author_meta['display_name'] = $currentuser->display_name; // Set display name from current user's data
+		$cc_author_meta['description'] = $currentuser->description; // Set bio from the current user's data
 	}
 	
 	/* Display the meta box contents */
 	?>
 	<div class="cc_author_metabox">
-		<label for="cc_author_displayname" class="selectit">Name</label>
-		<input type="text" name="cc_author_displayname" value="<?php echo esc_attr( $cc_author_displayname ); ?>" />
+		<label for="cc_author_meta[display_name]" class="selectit">Name</label>
+		<input type="text" name="cc_author_meta[display_name]" value="<?php echo esc_attr( $cc_author_meta['display_name'] ); ?>" />
 
-		<label for="cc_author_bio" class="selectit">Bio</label>
-		<textarea name="cc_author_bio" rows="5" cols="50" required><?php echo esc_attr( $cc_author_bio ); ?></textarea>
+		<label for="cc_author_meta[description]" class="selectit">Bio</label>
+		<textarea name="cc_author_meta[description]" rows="5" cols="50" required><?php echo esc_attr( $cc_author_meta['description'] ); ?></textarea>
 	</div>
 	<?php
-}
+} // cc_author_metabox( $post )
+
+/* Save the meta box data to post meta */
+function cc_author_save_meta( $post_id ) {
+	if ( isset( $_POST['cc_author_meta'] ) ) { // Verify that values have been provided
+		/* Save the metadata to the post */
+		update_post_meta( $post_id, '_cc_author_displayname', strip_tags( $_POST['cc_author_displayname'] ) );
+	}
+} // cc_author_save_meta( $post_id )
+add_action( 'save_post', 'cc_author_save_meta' ); // Hook WordPress to save meta data when saving post/page
 /**
  * End Author Meta Box
  */
