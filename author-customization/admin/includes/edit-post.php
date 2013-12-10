d0f2bfc03d366df1160b4935c20f47290f8d6377
Christiaan Conover <dev@cconover.com>
2013-12-10 03:55:49 -0500

    Added 'id' value to dropdown
    Added an HTML ID value for the post author dropdown

--- author-customization/admin/includes/edit-post.php
+++ author-customization/admin/includes/edit-post.php
@@ -74,6 +74,7 @@ function cc_author_metabox( $post ) {
 		if ( current_user_can( 'edit_others_posts' ) || current_user_can( 'edit_others_pages' ) ) { // Check the capabilities of the current user for sufficient privileges
 			wp_dropdown_users( array(
 				'name'			=> 'cc_author_postauthor', // Name for the form item
+				'id'			=> 'cc_author_postauthor', // Class for the form item
 				'selected'		=> $postauthorid // Select the post's author to be displayed by default
 			) );
 			?>
