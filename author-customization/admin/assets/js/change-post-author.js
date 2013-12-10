5c7fedd19d8a266e57d273837ee948855374a9da
Christiaan Conover <dev@cconover.com>
2013-12-10 03:53:40 -0500

    Initial code
    Initial code for dynamic author udpate

--- author-customization/admin/assets/js/change-post-author.js
+++ author-customization/admin/assets/js/change-post-author.js
@@ -1,2 +1,14 @@
-// JavaScript Document
+/*
+Dynamically update post author meta box fields when post author is changed
 
+Called by:
+/admin/includes/edit-post.php
+*/
+
+$( document ).ready( function() { // Don't execute anything until the page is loaded
+	$( "#cc_author_postauthor" ).change( function() {
+		var display_name = "Test";
+		$( "#cc_author_meta\\[0\\]\\[display_name\\]" ).val( display_name );
+		$( "#cc_author_meta\\[0\\]\\[description\\]" ).val( "Test bio" );
+	});
+});
