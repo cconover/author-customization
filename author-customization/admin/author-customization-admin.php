3a69c3b4e624804406f63d3311a039baecdf8d85
Christiaan Conover <dev@cconover.com>
2013-12-11 15:40:51 -0500

    Added check for 'admin-ajax.php'
    Added condition for admin-ajax.php in case an ajax call is made.

--- author-customization/admin/author-customization-admin.php
+++ author-customization/admin/author-customization-admin.php
@@ -201,7 +201,7 @@ function cc_author_options_page() {
  */
 
 /* If editing post, include the functions for use while editing a post */
-if ( strstr( $_SERVER['REQUEST_URI'], 'wp-admin/post-new.php' ) || strstr( $_SERVER['REQUEST_URI'], 'wp-admin/post.php' ) || strstr( $_SERVER['REQUEST_URI'], 'wp-admin/edit.php' ) ) {
+if ( strstr( $_SERVER['REQUEST_URI'], 'wp-admin/post-new.php' ) || strstr( $_SERVER['REQUEST_URI'], 'wp-admin/post.php' ) || strstr( $_SERVER['REQUEST_URI'], 'wp-admin/edit.php' ) || strstr( $_SERVER['REQUEST_URI'], 'wp-admin/admin-ajax.php' ) ) {
 	require_once( dirname( __FILE__ ) . '/includes/edit-post.php' ); // Retrieve file containing edit post functions
 }
 
