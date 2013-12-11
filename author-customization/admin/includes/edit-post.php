78ba088c650056438460c69faa4777abae6e7d54
Christiaan Conover <dev@cconover.com>
2013-12-11 17:13:50 -0500

    Cleaned up comments
    Cleaned up spacing of comments for better readability.

--- author-customization/admin/includes/edit-post.php
+++ author-customization/admin/includes/edit-post.php
@@ -46,12 +46,12 @@ function cc_author_add_metabox() {
 		plugins_url( 'assets/js/change-post-author.js', dirname( __FILE__ ) ),
 		'jquery'
 	);
-	wp_localize_script( // Localize script for AJAX calls
-		'cc-author-change-post-author', // Name of script call being localized
-		'authorchange', // AJAX object namespace, used to call values in the JS file
+	wp_localize_script(						// Localize script for AJAX calls
+		'cc-author-change-post-author',		// Name of script call being localized
+		'authorchange',						// AJAX object namespace, used to call values in the JS file
 		array(
-			'ajaxurl'	=> admin_url( 'admin-ajax.php' ), // URL for admin AJAX calls
-			'nonce'		=> wp_create_nonce( 'cc-author-change-author-nonce' ) // Nonce to authenticate request
+			'ajaxurl'	=> admin_url( 'admin-ajax.php' ),									// URL for admin AJAX calls
+			'nonce'		=> wp_create_nonce( 'cc-author-change-author-nonce' )				// Nonce to authenticate request
 		)
 	);
 } // cc_author_add_metabox()
