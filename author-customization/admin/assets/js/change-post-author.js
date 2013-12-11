6c3565b49cdf2624d5011204c4773d14c2b88d79
Christiaan Conover <dev@cconover.com>
2013-12-10 21:16:35 -0500

    Change test value
    Changed the test value that should be applied to the field.

--- author-customization/admin/assets/js/change-post-author.js
+++ author-customization/admin/assets/js/change-post-author.js
@@ -8,7 +8,7 @@ Called by:
 $( document ).ready( function() { // Don't execute anything until the page is loaded
 	$( "#cc_author_postauthor" ).change( function() {
 		var display_name = "Test";
-		$( "#cc_author_meta\\[0\\]\\[display_name\\]" ).val( display_name );
+		$( "#cc_author_meta\\[0\\]\\[display_name\\]" ).val( "New Name" );
 		$( "#cc_author_meta\\[0\\]\\[description\\]" ).val( "Test bio" );
 	});
 });
