39f452a534b40a6294be63f252843aaac4d599d9
Christiaan Conover <dev@cconover.com>
2013-12-11 18:52:16 -0500

    Changed case of TinyMCE object
    Changed case from ‘tinymce’ to ‘tinyMCE’

--- author-customization/admin/assets/js/change-post-author.js
+++ author-customization/admin/assets/js/change-post-author.js
@@ -27,7 +27,7 @@ jQuery( document ).ready( function( $ ) { // Don't execute anything until the pa
 				
 				/* Handle description update differently depending on whether 'wysiwyg' is enabled in plugin options */
 				if ( authormeta.wysiwyg == 'yes' ) {
-					tinyMCE.get( 'cc_author_meta\\[0\\]\\[description\\]' ).setContent( authormeta.description );
+					tinymce.get( 'cc_author_meta\\[0\\]\\[description\\]' ).setContent( authormeta.description );
 					console.log( "WYSIWYG is enabled and update attempted" );
 				}
 				else {
