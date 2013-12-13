ec961b7d6fb92f19f4aeecc0784b621997ec5f97
Christiaan Conover <dev@cconover.com>
2013-12-13 18:39:16 -0500

    Removed line break
    Removed unnecessary line break for better readability.

--- author-customization/author-customization.php
+++ author-customization/author-customization.php
@@ -35,7 +35,6 @@ function cc_author_displayname() {
 	global $post; // Access post data
 	
 	$postpage = get_option( 'cc_author_postpage' ); // Retrive plugin's post/page options
-	
 	$author = get_post_meta( $post->ID, '_cc_author_meta', true ); // Get the post-specific author metadata
 	
 	/* If the plugin setting is enabled and there's post-specific metadata stored and a post, page, or attachment is being displayed, show the post-specific display name. Otherwise use the profile display name. */
