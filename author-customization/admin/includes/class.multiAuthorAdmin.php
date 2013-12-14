ff4f31d903ae47c3044241cb65cf9a2de57218fb
Christiaan Conover <dev@cconover.com>
2013-12-13 21:53:58 -0500

    Initial code
    Initial code for class. Non-working as of this commit.

--- author-customization/admin/includes/class.multiAuthorAdmin.php
+++ author-customization/admin/includes/class.multiAuthorAdmin.php
@@ -4,6 +4,12 @@ Class for managing multiple authors for posts & pages inside WordPress admin
 */
 
 class multiAuthorAdmin {
+	/* Declare public variables */
+	public $cc_author_
 	
+	/* Class constructor */
+	function __construct() {
+		
+	}
 }
 ?>
