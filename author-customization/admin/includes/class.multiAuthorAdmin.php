ecc0183b8163fe4342d01ae0f6665217883f67d4
Christiaan Conover <dev@cconover.com>
2013-12-13 23:07:13 -0500

    Removed broken variable declaration
    Removed incomplete variable declaration.

--- author-customization/admin/includes/class.multiAuthorAdmin.php
+++ author-customization/admin/includes/class.multiAuthorAdmin.php
@@ -5,7 +5,7 @@ Class for managing multiple authors for posts & pages inside WordPress admin
 
 class multiAuthorAdmin {
 	/* Declare public variables */
-	public $cc_author_
+	
 	
 	/* Class constructor */
 	function __construct() {
