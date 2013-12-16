f35747f6cca17de54de3f73fcf2faa1ca415a3c1
Christiaan Conover <dev@cconover.com>
2013-12-16 18:56:24 -0500

    Late init function
    Created the function for late-execution init code.

--- author-customization/includes/class.multiAuthor.php
+++ author-customization/includes/class.multiAuthor.php
@@ -31,14 +31,31 @@ class multiAuthor {
 		
 	}
 	
+	/* Register the 'author' taxonomy. Delayed until late in initialization. */
+	function late_init() {
 		/* Register the author taxonomy */
-		$taxonomy_args = array( // Arguments for the register_taxonomy function
-			
+		$taxonomy_args = array(				// Arguments for the register_taxonomy function
+			'hierarchical'		=>	false,	// Non-hierarchical element
+			'label'				=>	false,	// No label needed, as this will not be shown in the UI
+			'rewrite'			=>	false,	// No rewrite needed, this taxonomy will piggyback off existing taxonomy
+			'query_var'			=>	false,	// No need to access this taxonomy through standard queries
+			'public'			=>	false,	// Do not display in UI
+			'sort'				=>	true	// Sort by the order stored in the term list
 		);
+		
+		/* Determine which post types can support author attribution, and set that list for use in taxonomy registration */
+		$post_types_with_author = array_values( get_post_types() ); // Get list of all available post types, and return them as numeric array
+		foreach ( $post_types_with_author as $key => $posttype ) { // Cycle through every post type
+			if ( !post_type_supports( $posttype, 'author' ) ) { // If post type does not support an author designation
+				unset ( $post_types_with_author[$key] ); // Remove the post type from the list of supported post types
+			}
+		}
+		$this->post_types = $post_types_with_author; // Set the class post types to the list created above
+		
 		register_taxonomy(
-			$this->author_taxonomy,		// Taxonomy name. This is the same as Co-Authors Plus for compatibility
-			$this->post_types,			// Taxonomy objects
-			$taxonomy_args				// Arguments for the taxonomy registration
+			$this->author_taxonomy,			// Taxonomy name. This is the same as Co-Authors Plus for compatibility
+			$this->post_types,				// Taxonomy objects
+			$taxonomy_args					// Arguments for the taxonomy registration
 		);
 		/* End register the author taxonomy */
 	}
