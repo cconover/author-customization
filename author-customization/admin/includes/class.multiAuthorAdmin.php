9fed3ebef6191f908cddf9914ed7d8352b822db6
Christiaan Conover <dev@cconover.com>
2013-12-16 12:33:39 -0500

    Updated public variables

--- author-customization/admin/includes/class.multiAuthorAdmin.php
+++ author-customization/admin/includes/class.multiAuthorAdmin.php
@@ -4,8 +4,7 @@ Class for managing multiple authors for posts & pages inside WordPress admin
 */
 
 class multiAuthorAdmin {
-	/* Declare public variables */
-	
+	public $cc_author_meta, $cc_author_postauthor; // Declare public variables
 	
 	/* Class constructor */
 	function __construct() {
