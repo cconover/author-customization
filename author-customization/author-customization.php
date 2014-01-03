667175d2b01284b725f014b5135d50b6f854a983
Christiaan Conover <dev@cconover.com>
2014-01-02 23:44:29 -0500

    Updated comment

--- author-customization/author-customization.php
+++ author-customization/author-customization.php
@@ -39,7 +39,7 @@ function cc_author_displayname( $post ) {
 		$name = 'Multiple Authors';
 	}
 	else { // If multiple authors support is not enabled in plugin options, run this code
-		$author = get_post_meta( $post->ID, '_cc_author_meta', true ); // Get the post-specific author metadata
+		$author = get_post_meta( $post->ID, '_cc_author_meta', true ); // Get the post-specific author metadata, if available
 		
 		/* If the plugin setting is enabled and there's post-specific metadata stored and a post, page, or attachment is being displayed, show the post-specific display name. Otherwise use the profile display name. */
 		if ( $author && !is_author() && isset( $postpage['perpost'] ) ) {
