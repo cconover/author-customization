e5cd4d244ffe32f46c100caefac87745936d09d6
Christiaan Conover <dev@cconover.com>
2014-01-07 15:38:28 -0500

    Changed post ID reference

--- author-customization/author-customization.php
+++ author-customization/author-customization.php
@@ -40,7 +40,7 @@ function cc_author_displayname() {
 		$name = 'Multiple Authors';
 	}
 	else { // If multiple authors support is not enabled in plugin options, run this code
-		$author = get_post_meta( $post->ID, '_cc_author_meta', true ); // Get the post-specific author metadata, if available
+		$postmeta = get_post_meta( get_the_ID(), '_cc_author_meta', true ); // Get the post-specific author metadata, if available
 		
 		/* If the plugin setting is enabled and there's post-specific metadata stored and a post, page, or attachment is being displayed, show the post-specific display name. Otherwise use the profile display name. */
 		if ( $author && !is_author() && isset( $postpage['perpost'] ) ) {
@@ -73,7 +73,7 @@ function cc_author_description() {
 		
 	}
 	else { // If multiple authors support is not enabled in plugin options, run this code
-		$author = get_post_meta( $post->ID, '_cc_author_meta', true ); // Get the post-specific author metadata
+		$author = get_post_meta( get_the_ID(), '_cc_author_meta', true ); // Get the post-specific author metadata
 		
 		/* If the plugin setting is enabled and there's post-specific metadata stored and a post, page, or attachment is being displayed, show the post-specific bio. Otherwise use the profile bio. */
 		if ( $author && isset( $postpage['perpost'] ) ) {
