a7eaaa8ab87e5795a32f02162a5b91662f9c91d6
Christiaan Conover <dev@cconover.com>
2014-01-08 12:08:50 -0500

    Remove functionality for creating new author
    Removed the functionality to create a new WordPress author from within
    the post meta box.

--- author-customization/admin/assets/js/edit-post.js
+++ author-customization/admin/assets/js/edit-post.js
@@ -38,62 +38,6 @@ jQuery( document ).ready( function( $ ) { // Don't execute anything until the pa
 			} // function( jsonString )
 		); // $.post
 	}); // $( "#cc_author_postauthor" ).change()
-
-	$( "#cc_author_create_author" ).click( function() {
-		$( "#cc-author-metabox .spinner" ).css( 'display', 'inline-block' ); // Display the 'loading' spinner
-		
-		/* Author data to pass to the server */
-		var authorinfo = {
-			action : 'cc_author_create_author',
-			nonce : cc_author_edit_post.nonce,
-			cc_author_create : {
-				first_name : $( "#cc_author_create\\[first_name\\]" ).val(),
-				last_name : $( "#cc_author_create\\[last_name\\]" ).val(),
-				email : $( "#cc_author_create\\[email\\]" ).val()
-			}
-		};
-		
-		var requestform = {
-			action : 'cc_author_create_user_metabox_request_form',
-			nonce : cc_author_edit_post.nonce,
-			newuserform : 'yes'
-		};
-		
-		$.post(
-			cc_author_edit_post.ajaxurl,
-			requestform,
-			function( response ) {
-				var newuserform = response; // Parse the JSON sent back from the server
-				
-				$( "#cc_author_metabox" ).hide(); // Hide the standard metabox fields
-				
-				$( "#cc-author-metabox .inside" ).append( response ); // Show new user form
-				console.log( response ); // Output server info to console
-				
-				$( "#cc-author-metabox .spinner" ).css( 'display', 'none' ); // Hide the 'loading' spinner
-			}
-		);
-	}); // $( "#cc_author_create_author" ).click()
-	
-	$( "#cc_author_create_submit" ).click( function() {
-		/* Send POST request to the server and process the response */
-		$.post(
-			cc_author_edit_post.ajaxurl,
-			authorinfo,
-			function( jsonString ) {
-				var newauthor = $.parseJSON( jsonString ); // Parse the JSON returned from the server
-			}
-		);
-	} ); // $( "#cc_author_create_submit" ).click()
-	
-	$( "#cc_author_create_cancel" ).click( function() {
-		$( "#cc-author-metabox .spinner" ).css( 'display', 'inline-block' ); // Display the 'loading' spinner
-		
-		$( "#cc_author_create_metabox" ).remove(); // Remove the meta box for creating a new author
-		$( "#cc_author_metabox" ).show(); // Show the normal author box that was previously hidden
-		
-		$( "#cc-author-metabox .spinner" ).css( 'display', 'none' ); // Hide the 'loading' spinner
-	} ); // $( "#cc_author_create_cancel" ).click()
 	
 	$( "#cc_author_javascript" ).val( "yes" ); // Set JavaScript field to 'yes' so the server can know whether JavaScript is working
 }); // jQuery ready check
