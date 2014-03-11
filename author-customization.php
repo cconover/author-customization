9872ca17f931aa192a1ef7cc9b9202189d019d79
cconover <dev@cconover.com>
2014-03-11 04:30:46 +0000

    Added display name and description methods to main plugin class

--- author-customization.php
+++ author-customization.php
@@ -35,10 +35,18 @@ class cc_author {
 	protected $pluginfile;
 	
 	/* Plugin's class constructor */
-	public function __construct() {
+	function __construct() {
 		// Set plugin variables
 		$this->pluginpath = dirname( __FILE__ );
 		$this->pluginfile = __FILE__;
+		
+		/* Plugin hooks and filters */
+		// Display name and description
+		if ( !is_admin() ) { // Only add filters if not in admin
+			add_filter( 'the_author', array( &$this, 'displayname' ) ); // Hook display name function into 'the_author' filter
+			add_filter( 'get_the_author_display_name', array( &$this, 'displayname' ) ); // Hook display name function into 'get_the_author_display_name' filter
+			add_filter( 'get_the_author_description', array( &$this, 'description' ) ); // Hook description into 'get_the_author_description' filter
+		}
 		
 		// Load admin class if in admin
 		if ( is_admin() ) {
@@ -50,6 +58,70 @@ class cc_author {
 			register_deactivation_hook( $this->pluginfile, array( &$admin, 'deactivate' ) );
 		}
 	}
+	
+	// Display the author name
+	public function displayname( $post ) {
+		global $post;
+		
+		$postpage = get_option( 'cc_author_postpage' ); // Retrive plugin's post/page options
+	
+		if ( isset( $postpage['multiple-authors'] ) ) { // If multiple authors support is enabled in plugin options, run this code
+			$name = 'Multiple Authors';
+		}
+		else { // If multiple authors support is not enabled in plugin options, run this code
+			$postmeta = get_post_meta( $post->ID, '_cc_author_meta', true ); // Get the post-specific author metadata, if available
+			
+			/* If the plugin setting is enabled and there's post-specific metadata stored and a post, page, or attachment is being displayed, show the post-specific display name. Otherwise use the profile display name. */
+			if ( $postmeta && !is_author() && isset( $postpage['perpost'] ) ) {
+				foreach ( $postmeta as $authormeta ) {
+					foreach ( $authormeta as $key => $meta ) {
+						$name = $authormeta['display_name']; // Set the name to the display name stored for the post
+					}
+				}
+			}
+			else {
+				$author = get_userdata( $post->post_author ); // Get the profile data for the post author
+				$name = $author->display_name; // Set the display name to the value stored in the author's profile
+			}
+		}
+	
+		return $name; // Send the name back to WordPress for displaying on the post
+	} // End displayname()
+	
+	public function description( $post ) {
+		global $post;
+	
+		$postpage = get_option( 'cc_author_postpage' ); // Get plugin options for posts/pages
+	
+		if ( isset( $postpage['multiple-authors'] ) ) { // If multiple authors support is enabled in plugin options, run this code
+			
+		}
+		else { // If multiple authors support is not enabled in plugin options, run this code
+			$author = get_post_meta( $post->ID, '_cc_author_meta', true ); // Get the post-specific author metadata
+			
+			/* If the plugin setting is enabled and there's post-specific metadata stored and a post, page, or attachment is being displayed, show the post-specific bio. Otherwise use the profile bio. */
+			if ( $author && isset( $postpage['perpost'] ) ) {
+				foreach ( $author as $authormeta ) {
+					foreach ( $authormeta as $key => $meta ) {
+						$description = $authormeta['description']; // Set the description to the one saved in the post metadata
+					}
+				}
+			}
+			else {
+				$author = get_userdata( $post->post_author ); // Get the profile data for the post author
+				$description = $author->description; // Set the description to the value stored in the author's profile
+			}
+		
+			/* If 'relnofollow' is set, add rel="nofollow" to links in bio */
+			if ( isset( $postpage['relnofollow'] ) ) {
+				$description = str_replace( 'href', 'rel="nofollow" href', $description );
+			}
+			
+			$description = apply_filters( 'the_content', $description ); // Enable formatting for bio
+		}
+		
+		return $description; // Send back the description for WordPress to display
+	} // End description()
 }
 /**
  * End cc_author
