97ea9680ea7104eb296f987db05a29de4de134d7
cconover <dev@cconover.com>
2014-03-31 06:03:12 +0000

    Changed form selectors from using 'id' to 'name'

--- admin/assets/js/edit-post.js
+++ admin/assets/js/edit-post.js
@@ -5,18 +5,18 @@ Dynamically update post author meta box fields when post author is changed
 // Don't execute anything until the page is loaded
 jQuery( document ).ready( function( $ ) {
 	// Show the meta box form (not shown by default; requires JavaScript for it to be visible)
-	$( "#cc_author_metabox" ).show();
+	$( '#cc_author_metabox' ).show();
 	
 	// If the author dropdown value is changed, execute the script
-	$( "#cc_author_postauthor" ).change( function() {
+	$( '[name="cc_author_postauthor"]' ).change( function() {
 		// Display the 'loading' spinner
-		$( "#cc-author-metabox .spinner" ).css( 'display', 'inline-block' );
+		$( '#cc-author-metabox .spinner' ).css( 'display', 'inline-block' );
 		
 		// Data to pass to the server. Called below during $.post()
 		var data = {
 			action : 'cc_author_change_postauthor', // Action hook for the server-side callback
 			nonce : cc_author_edit_post.nonce, // Nonce received from server to authenticate request
-			authorID : $( "#cc_author_postauthor" ).val() // author ID for retrieving profile data
+			authorID : $( '#cc_author_postauthor' ).val() // author ID for retrieving profile data
 		};
 		
 		// Send request to the server and process response
@@ -27,23 +27,23 @@ jQuery( document ).ready( function( $ ) {
 				var authormeta = $.parseJSON( jsonString ); // Parse the JSON received from the server response
 				
 				// Change the value of the author display name to the value received from the server
-				$( "#cc_author_meta\\[0\\]\\[display_name\\]" ).val( authormeta.display_name );
+				$( '[name="cc_author_meta\\[0\\]\\[display_name\\]"]' ).val( authormeta.display_name );
 				
 				// If 'wysiwyg' is enabled in plugin options, update the author bio through TinyMCE
-				if ( authormeta.wysiwyg == 'yes' ) {
-					tinymce.get( 'cc_author_meta[0][description]' ).setContent( authormeta.description );
+				if ( authormeta.wysiwyg == 'yes' && typeof tinymce == 'object' ) {
+					tinymce.get( 'cc_author_meta_description' ).setContent( authormeta.description );
 				}
 				// If it's not enabled, update the standard textarea
 				else {
-					$( "#cc_author_meta\\[0\\]\\[description\\]" ).val( authormeta.description ); // Change the value of the author bio to the value received from the server
+					$( '[name="cc_author_meta\\[0\\]\\[description\\]"]' ).val( authormeta.description ); // Change the value of the author bio to the value received from the server
 				}
 				
 				// Hide the 'loading' spinner
-				$( "#cc-author-metabox .spinner" ).css( 'display', 'none' );
+				$( '#cc-author-metabox .spinner' ).css( 'display', 'none' );
 			} // function( jsonString )
 		); // $.post
-	}); // $( "#cc_author_postauthor" ).change()
+	}); // $( '#cc_author_postauthor' ).change()
 	
 	// Set JavaScript field to 'yes' so the server can know whether JavaScript is working
-	$( "#cc_author_javascript" ).val( "yes" );
+	$( '[name=cc_author_javascript]' ).val( 'yes' );
 }); // jQuery ready check
